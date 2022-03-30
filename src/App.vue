09469f661508a7dc9d2374fa17e526202866aa08
小沈曰 <409178030@qq.com>
2022-03-31 00:29:26 +0800

    项目搭建-配置路径别名

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,7 @@
+<script lang="ts">
+import request from './utils/request'
+request.get('/home/index').then((res) => {
+  console.log(res)
+})
+</script>
 <template>app</template>
---
+++ src/utils/request.ts
@@ -0,0 +1,45 @@
+// import Message from '@/components/message'
+// import useStore from '@/store'
+
+import axios, { AxiosError } from 'axios'
+export const baseURL = 'https://apipc-xiaotuxian-front.itheima.net/'
+const request = axios.create({
+  baseURL,
+  // baseURL: 'http://pcapi-xiaotuxian-front.itheima.net/',
+  timeout: 5000,
+})
+
+// 添加请求拦截器
+request.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    // const { user } = useStore()
+    // if (user.profile.token && config.headers) {
+    //   config.headers.Authorization = `Bearer ${user.profile.token}`
+    // }
+    return config
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+request.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error: AxiosError) {
+    // Message.error(error.response.data.message)
+    // 对响应错误做点什么
+    // if (!error.response) {
+    //   Message.error('网络异常，请稍后重置')
+    // } else {
+    //   Message.error(error.response.data.message)
+    // }
+    return Promise.reject(error)
+  }
+)
+
+export default request
