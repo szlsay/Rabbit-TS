135c8a316ba7fa14f31335140e71be943e490610
小沈曰 <409178030@qq.com>
2022-03-31 13:37:11 +0800

    layout布局-底部组件封装

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts" name="Layout">
 import AppTopnav from './components/app-topnav.vue'
 import AppHeader from './components/app-header.vue'
+import AppFooter from './components/app-footer.vue'
 </script>
 
 <template>
   <div>
     <app-topnav></app-topnav>
     <app-header></app-header>
+    <app-footer></app-footer>
   </div>
 </template>
 
