b97d6f4d9e1cfb3bbf312d0656acac438c9c335e
小沈曰 <409178030@qq.com>
2022-03-31 13:33:59 +0800

    Layout布局-头部布局功能

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts" name="Layout">
 import AppTopnav from './components/app-topnav.vue'
+import AppHeader from './components/app-header.vue'
 </script>
 
 <template>
   <div>
     <app-topnav></app-topnav>
+    <app-header></app-header>
   </div>
 </template>
 
