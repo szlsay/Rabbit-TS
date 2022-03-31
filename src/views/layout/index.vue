a9482f75d3a3c45d344a628dc42ee8a6359ec0d1
小沈曰 <409178030@qq.com>
2022-03-31 16:35:28 +0800

    布局组件-吸顶效果重构

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts" name="Layout">
 import AppTopnav from './components/app-topnav.vue'
 import AppHeader from './components/app-header.vue'
+import AppHeaderSticky from './components/app-header-sticky.vue'
 import AppFooter from './components/app-footer.vue'
 </script>
 
@@ -8,6 +9,7 @@ import AppFooter from './components/app-footer.vue'
   <div>
     <app-topnav></app-topnav>
     <app-header></app-header>
+    <app-header-sticky></app-header-sticky>
     <div class="app-body">
       <!-- 路由出口 -->
       <RouterView></RouterView>
