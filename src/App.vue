131c45c5094728c675bdc22e785ef99df9360fe0
小沈曰 <409178030@qq.com>
2022-03-31 12:55:17 +0800

    项目搭建-样式处理-公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <script lang="ts"></script>
 <template>
   <h1>app</h1>
+  <ul>
+    <li>1</li>
+    <li>1</li>
+    <li>1</li>
+    <li>1</li>
+  </ul>
+  <ol>
+    <li>2</li>
+    <li>2</li>
+    <li>2</li>
+    <li>2</li>
+  </ol>
 </template>
 <style lang="less" scoped>
 // 1. 在vite中，样式中使用@不需要~
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
+// 引入通用样式，浏览器表现是一致的
+import 'normalize.css'
+// 引入项目中一些通用样式
+import '@/assets/styles/common.less'
 
 createApp(App).mount('#app')
