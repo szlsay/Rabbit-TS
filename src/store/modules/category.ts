f17a76ef1869a593ecea668fcb020fcb8ac79335
小沈曰 <409178030@qq.com>
2022-03-31 16:03:32 +0800

    布局组件-分类导航的优化

--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,10 +1,16 @@
 import { defineStore } from "pinia";
 import request from '@/utils/request'
 import { ApiRes, CategoryItem } from '@/types/data'
+import { topCategory } from '../constants'
+const defaultCategory = topCategory.map((item) => { 
+  return {
+    name: item
+  }
+})
 export default defineStore('category', {
   state: () => ({ 
     count: 10,
-    list: [] as (CategoryItem)[]
+    list: defaultCategory as (CategoryItem)[]
   }),
   actions: {
     async getAllCategory() {
