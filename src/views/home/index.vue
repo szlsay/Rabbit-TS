2e979a4482619ab13c437f5836158fa759369510
小沈曰 <409178030@qq.com>
2022-03-31 22:04:31 +0800

    首页-最新专题功能完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,6 +3,9 @@ import HomeBanner from './components/home-banner.vue'
 import HomeCategory from './components/home-category.vue'
 import HomeNew from './components/home-new.vue'
 import HomeHot from './components/home-hot.vue'
+import HomeBrand from './components/home-brand.vue'
+import HomeProduct from './components/home-product.vue'
+import HomeSpecial from './components/home-special.vue'
 </script>
 <template>
   <div class="page-home">
@@ -17,12 +20,12 @@ import HomeHot from './components/home-hot.vue'
       <HomeNew></HomeNew>
       <!-- 人气推荐 -->
       <HomeHot></HomeHot>
-      <!-- <HomePanel title="新鲜好物" sub-title="我是子标题">
-        <template #right>
-          <XtxMore></XtxMore>
-        </template>
-        我是内容
-      </HomePanel> -->
+      <!-- 热门品牌 -->
+      <HomeBrand></HomeBrand>
+      <!-- 商品区块 -->
+      <HomeProduct></HomeProduct>
+      <!-- 最新专题 -->
+      <HomeSpecial />
     </div>
   </div>
 </template>
