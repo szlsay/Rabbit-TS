03605a0ea9f4305ab3ed96dfab4686dcbce78201
小沈曰 <409178030@qq.com>
2022-03-31 12:52:40 +0800

    样式处理-自动导入

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 </template>
 <style lang="less" scoped>
 // 1. 在vite中，样式中使用@不需要~
-@import '@/assets/styles/variables.less';
-@import '@/assets/styles/mixins.less';
+// @import '@/assets/styles/variables.less';
+// @import '@/assets/styles/mixins.less';
 h1 {
   color: @xtxColor;
   background-color: @warnColor;
