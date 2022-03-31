743919a03c2a864ba64f4b900f771652474f12a1
小沈曰 <409178030@qq.com>
2022-03-31 12:49:11 +0800

    项目搭建-样式-通用变量和函数

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script lang="ts">
-import request from './utils/request'
-request.get('/home/index').then((res) => {
-  console.log(res)
-})
-</script>
-<template>app</template>
+<script lang="ts"></script>
+<template>
+  <h1>app</h1>
+</template>
+<style lang="less" scoped>
+// 1. 在vite中，样式中使用@不需要~
+@import '@/assets/styles/variables.less';
+@import '@/assets/styles/mixins.less';
+h1 {
+  color: @xtxColor;
+  background-color: @warnColor;
+  .hoverShadow();
+}
+</style>
