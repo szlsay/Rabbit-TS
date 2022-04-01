bd2b91859685bd00f90404bdb8178055dae64629
小沈曰 <409178030@qq.com>
2022-04-01 18:30:44 +0800

    顶级分类-渲染商品数据

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,21 +1,24 @@
 <script lang="ts" name="TopCategory" setup>
-import { useRoute } from 'vue-router'
 import useStore from '@/store'
-import { watch, watchEffect } from 'vue'
-const route = useRoute()
+import { storeToRefs } from 'pinia'
+import { watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+import GoodsItem from './components/goods-item.vue'
+
 const { category, home } = useStore()
 
+// 获取当前分类的id
+const route = useRoute()
 // watch(
 //   () => route.params.id,
 //   (value) => {
-//     console.log('change:', value)
-//     category.getTopCategory(route.params.id as string)
+//     console.log(value, '变化')
+//     category.getTopCategory(value as string)
 //   },
 //   {
 //     immediate: true,
 //   }
 // )
-
 watchEffect(() => {
   // 只有是一级分类的情况下，才发送这个请求
   // console.log(route.fullPath)
@@ -27,14 +30,17 @@ watchEffect(() => {
     home.getBannerList()
   }
 })
+
+const { topCategory } = storeToRefs(category)
 </script>
 
 <template>
   <div class="top-category">
     <div class="container">
+      <!-- 渲染面包屑导航 -->
       <XtxBread>
         <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem>{{ category.topCategory.name }}</XtxBreadItem>
+        <XtxBreadItem>{{ topCategory.name }}</XtxBreadItem>
       </XtxBread>
 
       <!-- 轮播图 -->
@@ -43,6 +49,40 @@ watchEffect(() => {
         style="height: 500px"
         auto-play
       ></XtxCarousel>
+
+      <!-- 所有二级分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="item in topCategory.children" :key="item.id">
+            <RouterLink :to="`/category/sub/${item.id}`">
+              <img v-lazy="item.picture" />
+              <p>{{ item.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+
+      <!-- 商品列表 -->
+      <!-- 分类关联商品 -->
+      <div
+        class="ref-goods"
+        v-for="item in topCategory.children"
+        :key="item.id"
+      >
+        <div class="head">
+          <h3>- {{ item.name }} -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem
+            v-for="goods in item.goods"
+            :key="goods.id"
+            :goods="goods"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
