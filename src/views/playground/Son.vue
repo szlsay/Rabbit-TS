197622720eb486bab1f7f4d6ef755de01f02bcfd
小沈曰 <409178030@qq.com>
2022-04-01 22:54:19 +0800

    defineEmits

---
+++ src/views/playground/Son.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps({
+  money: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const emitmoney = defineEmits(['update:money'])
+</script>
+
+<template>
+  <h3>子组件-{{ money }}</h3>
+  <button @click="emitmoney('update:money', money + 1)">+1</button>
+</template>
+
+<style scoped lang="less"></style>
---
+++ src/views/playground/watcheffect.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { ref, watchEffect } from 'vue'
+
+const money = ref(100)
+const name = ref('zs')
+
+watchEffect(() => {
+  console.log(name.value, '变化了')
+  console.log(money.value, '变化了')
+})
+</script>
+
+<template>
+  <h3>哈哈哈</h3>
+  <button @click="money++">加钱</button>
+  <button @click="name = 'ls'">改名</button>
+</template>
+
+<style scoped lang="less">
+.box {
+  width: 40px;
+}
+
+.aa-enter-active {
+  animation: heartBeat 0.3s linear;
+}
+.aa-leave-active {
+  animation: heartBeat 0.3s linear reverse;
+}
+
+@keyframes heartBeat {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
